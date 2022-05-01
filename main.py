bc5419f7f72951219f988155860929dd44e9abcb
Alwaz18 <alwazk8@gmail.com>
2022-05-01 13:19:39 +0500

    pycharm

--- main.py
+++ main.py
@@ -1,100 +1,71 @@
+from datetime import datetime
+import json
+
 from fastapi import (
     FastAPI, WebSocket, WebSocketDisconnect,
     Request
 )
-from pydantic import BaseModel
 import uvicorn
-
-# from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # can alter with time
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# define endpoint
+
+# from typing import List if py < 10
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
 @app.get("/")
 def home():
     return "Welcome Home"
 
 
-# manager
-class SocketManager:
-    def __init__(self):
-        self.active_connections: list[(WebSocket, str)] = []
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # await manager.send_personal_message(f"You wrote: {data}", websocket)
+            message = {"time": current_time, "clientId": client_id, "message": data}
+            await manager.broadcast(json.dumps(message))
 
-    async def connect(self, websocket: WebSocket, user: str):
-        await websocket.accept()
-        self.active_connections.append((websocket, user))
-
-    def disconnect(self, websocket: WebSocket, user: str):
-        self.active_connections.remove((websocket, user))
-
-    async def broadcast(self, data):
-        for connection in self.active_connections:
-            await connection[0].send_json(data)
-
-
-manager = SocketManager()
-
-
-@app.websocket("/api/chat")
-async def chat(websocket: WebSocket):
-    sender = websocket.cookies.get("X-Authorization")
-    if sender:
-        await manager.connect(websocket, sender)
-        response = {
-            "sender": sender,
-            "message": "got connected"
-        }
-        await manager.broadcast(response)
-        try:
-            while True:
-                data = await websocket.receive_json()
-                await manager.broadcast(data)
-        except WebSocketDisconnect:
-            manager.disconnect(websocket, sender)
-            response['message'] = "left"
-            await manager.broadcast(response)
-
-
-@app.get("/api/current_user")
-def get_user(request: Request):
-    return request.cookies.get("X-Authorization")
-
-
-class RegisterValidator(BaseModel):
-    username: str
-
-
-class Response(BaseModel):
-    # username: str
-    # password: str
-    pass
-
-
-@app.post("/api/register")
-def register_user(user: RegisterValidator, response: Response):
-    response.set_cookie(key="X-Authorization", value=user.username, httponly=True)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        message = {"time": current_time, "clientId": client_id, "message": "Offline"}
+        await manager.broadcast(json.dumps(message))
 
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True, access_log=True)
-
-#
-# @app.websocket("/api/chat")
-# async def chat(websocket: WebSocket):
-#     sender = websocket.cookies.get("X-Authorization")
-#     if sender:
-#         await manager.connect(websocket, sender)
-#         response = {
-#             "sender": sender,
-#             "message": "got connected"
-#         }
-#         await manager.broadcast(response)
-#         try:
-#             while True:
-#                 data = await websocket.receive_json()
-#                 await manager.broadcast(data)
-#         except WebSocketDisconnect:
-#             manager.disconnect(websocket, sender)
-#             response['message'] = "left"
-#             await manager.broadcast(response)
